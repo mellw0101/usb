617f4d4ce53bb1d41e622f9d2684a9c149a1d63d
mellw0101 <melwivensson@gmail.com>
2023-12-22 05:33:13 -0800

    Changes to be committed: 	modified:   mwm/main.cpp

--- mwm/main.cpp
+++ mwm/main.cpp
@@ -2604,7 +2604,7 @@ class Event {
                     {
                         client * c = get::client_from_win(& e->event);
                         XCBAnimator anim(conn, c->win);
-                        anim.move(c->x, c->y, c->x + 1200, c->y, 1000);
+                        anim.move(c->x, c->y, c->x + screen->width_in_pixels, c->y, 1000);
                         wm::update_client(c);
                         break;
                     }
