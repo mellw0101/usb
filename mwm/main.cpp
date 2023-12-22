5c4ce0eb93d48d72d6b7146e65da8f169154a973
mellw0101 <melwivensson@gmail.com>
2023-12-22 03:35:01 -0800

    Changes to be committed: 	modified:   mwm/main.cpp

--- mwm/main.cpp
+++ mwm/main.cpp
@@ -916,7 +916,7 @@ class Animate {
             std::this_thread::sleep_for(std::chrono::milliseconds(time));
 
             // Stop the animation
-            // stopAnimation();
+            stopAnimation();
 			wm::update_client(c);
         }
 
