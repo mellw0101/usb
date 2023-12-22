4d5e90207719a056713a73c6e4f16ec0433f7632
mellw0101 <melwivensson@gmail.com>
2023-12-22 03:45:35 -0800

    Changes to be committed: 	modified:   mwm/main.cpp

--- mwm/main.cpp
+++ mwm/main.cpp
@@ -908,7 +908,7 @@ class Animate {
 			log_info("stepX = " + std::to_string(stepX));
 
             // Start a new thread for animation
-            animationThread = std::thread(&Animate::animateThread, endX, endY, stepX, stepY, steps);
+            std::thread animationThread(&Animate::animateThread, endX, endY, stepX, stepY, steps);
 
             // Wait for the animation to complete
             std::this_thread::sleep_for(std::chrono::milliseconds(time));
