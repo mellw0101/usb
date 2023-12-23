789b56160a45694a25ab2ae51a3c1d56db8ec1b8
mellw0101 <melwivensson@gmail.com>
2023-12-23 07:46:47 -0800

    Changes to be committed: 	modified:   mwm/main.cpp

--- mwm/main.cpp
+++ mwm/main.cpp
@@ -1134,7 +1134,7 @@ class XCPPBAnimator
         std::chrono::high_resolution_clock::time_point HlastUpdateTime;
         
         /* FRAMERATE */
-        const double frameRate = 120.0;
+        const double frameRate = 600.0;
         
         /* DURATION IN MILLISECONDS THAT EACH FRAME SHOULD LAST */
         const double frameDuration = 1000.0 / frameRate; 
